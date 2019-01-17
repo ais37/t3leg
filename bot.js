c0790d5b274e35a01a45f8197e9ed4bf7f9f3bec
shadowssxx <43233768+shadowssxx@users.noreply.github.com>
2019-01-17 14:57:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-﻿var Discord = require('discord.js');
+var Discord = require('discord.js');
 var client = new Discord.Client();
 
 client.on('message', message => {
   if (!message.guild) return;
 
-  if (message.content === 'shadow1') {
+  if (message.content === 'shadow5') {
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
         .then(connection => { 
@@ -16,4 +16,40 @@ client.on('message', message => {
     }
   }
 });
+
+client2.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content === 'shadow5') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('لقد دخلت الروم بنجاح !');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('يجب ان تكون في روم صوتي');
+    }
+  }
+});
+
+client3.on('message', message => {
+  if (!message.guild) return;
+
+  if (message.content === 'shadow5') {
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { 
+          message.reply('لقد دخلت الروم بنجاح !');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('يجب ان تكون في روم صوتي');
+    }
+  }
+});
+
+
   client.login(process.env.BOT_TOKEN);
+  client2.login(process.env.BOT_TOKEN2);
+  client3.login(process.env.BOT_TOKEN3);
